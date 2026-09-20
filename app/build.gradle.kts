4a5986ba10dbbd374efe60d42690f309e96ff34f
SamSo...? <samsosleepy@gmail.com>
2026-09-20 22:22:22 +0700

    Bump Android package version for launcher icon refresh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.voicecraft.vcmumbleserver"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0-beta.1"
+        versionCode = 7
+        versionName = "0.6.0-beta.2"
 
         buildConfigField("boolean", "VC_MUMBLE_CORE", vcMumbleCore.toString())
 
