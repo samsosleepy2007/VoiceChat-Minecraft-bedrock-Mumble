bc10b9e8a3b83ed22a5e5eeba141798c15b247d7
SamSo...? <samsosleepy@gmail.com>
2026-09-21 04:12:38 +0700

    Prepare beta.6 attenuation release with combined checksums

--- scripts/check-project.sh
+++ scripts/check-project.sh
@@ -194,8 +194,8 @@ grep -Fq 'android.permission.WAKE_LOCK' app/src/main/AndroidManifest.xml
 grep -Fq 'android:stopWithTask="false"' app/src/main/AndroidManifest.xml
 grep -Fq 'android:icon="@mipmap/ic_launcher_sleepy"' app/src/main/AndroidManifest.xml
 grep -Fq 'android:roundIcon="@mipmap/ic_launcher_sleepy"' app/src/main/AndroidManifest.xml
-grep -Fq 'versionCode = 10' app/build.gradle.kts
-grep -Fq 'versionName = "0.6.0-beta.5"' app/build.gradle.kts
+grep -Fq 'versionCode = 11' app/build.gradle.kts
+grep -Fq 'versionName = "0.6.0-beta.6"' app/build.gradle.kts
 test ! -e app/src/main/res/drawable-nodpi/ic_launcher.png
 test ! -e app/src/main/res/drawable-nodpi/ic_launcher_sleepy.png
 grep -Fq 'PowerManager.PARTIAL_WAKE_LOCK' app/src/core/java/com/voicecraft/vcmumbleserver/MumbleServerService.java
