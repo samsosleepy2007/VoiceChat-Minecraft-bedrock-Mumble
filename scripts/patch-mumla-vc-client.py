ef4065d97ebe0a28f6bf32e86c27d2fdb0702114
SamSo...? <samsosleepy@gmail.com>
2026-09-25 01:35:02 +0700

    Avoid Settings class collision in VC Mumla battery flow

--- scripts/patch-mumla-vc-client.py
+++ scripts/patch-mumla-vc-client.py
@@ -447,8 +447,7 @@ def patch_battery_unrestricted(
             activity,
             "import android.os.IBinder;\n",
             "import android.os.IBinder;\n"
-            "import android.os.PowerManager;\n"
-            "import android.provider.Settings;\n",
+            "import android.os.PowerManager;\n",
             "MumlaActivity battery imports",
         )
         activity = replace_once(
@@ -546,7 +545,7 @@ def patch_battery_unrestricted(
     private void requestVcBatteryUnrestricted() {
         try {
             Intent request = new Intent(
-                    Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                    android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
                     Uri.parse("package:" + getPackageName())
             );
             startActivity(request);
@@ -557,10 +556,10 @@ def patch_battery_unrestricted(
 
     private void openVcBatteryOptimizationSettings() {
         try {
-            startActivity(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
+            startActivity(new Intent(android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
         } catch (Exception error) {
             startActivity(new Intent(
-                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
                     Uri.parse("package:" + getPackageName())
             ));
         }
@@ -582,8 +581,7 @@ def patch_battery_unrestricted(
             "import android.net.Uri;\n"
             "import android.os.Build;\n"
             "import android.os.Bundle;\n"
-            "import android.os.PowerManager;\n"
-            "import android.provider.Settings;\n",
+            "import android.os.PowerManager;\n",
             "GeneralSettingsFragment battery imports",
         )
         fragment = replace_once(
@@ -646,16 +644,16 @@ def patch_battery_unrestricted(
         }
         try {
             Intent request = new Intent(
-                    Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                    android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
                     Uri.parse("package:" + requireContext().getPackageName())
             );
             startActivity(request);
         } catch (Exception error) {
             try {
-                startActivity(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
+                startActivity(new Intent(android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
             } catch (Exception ignored) {
                 startActivity(new Intent(
-                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
                         Uri.parse("package:" + requireContext().getPackageName())
                 ));
             }
