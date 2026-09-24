eb26523a0ed7aef5c88dd8c0da7d331546c7a0e0
SamSo...? <samsosleepy@gmail.com>
2026-09-25 00:31:29 +0700

    Bump Android app to v0.6.0-beta.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.voicecraft.vcmumbleserver"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.6.0-beta.8"
+        versionCode = 14
+        versionName = "0.6.0-beta.9"
 
         buildConfigField("boolean", "VC_MUMBLE_CORE", vcMumbleCore.toString())
 
